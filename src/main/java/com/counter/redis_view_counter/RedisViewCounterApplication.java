79e714ddd8635250ee6b223e35e35306d042e9d2
jisooou <jisoou4@gmail.com>
2026-03-11 13:12:57 +0900

    chore: Gradle 프로젝트 루트 파일 추가

---
+++ src/main/java/com/counter/redis_view_counter/RedisViewCounterApplication.java
@@ -0,0 +1,13 @@
+package com.counter.redis_view_counter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisViewCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisViewCounterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/counter/redis_view_counter/RedisViewCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.counter.redis_view_counter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisViewCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
