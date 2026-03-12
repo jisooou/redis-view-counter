9a946c0c8e63e8abb1175bd296abea81997065f8
jisooou <jisoou4@gmail.com>
2026-03-13 00:05:34 +0900

    feat: redis로 조회수 증가 로직으로 수정

--- src/main/java/com/counter/redis_view_counter/place/service/PlaceService.java
+++ src/main/java/com/counter/redis_view_counter/place/service/PlaceService.java
@@ -5,6 +5,7 @@ import com.counter.redis_view_counter.place.dto.PlaceResponseDto;
 import com.counter.redis_view_counter.place.entity.Place;
 import com.counter.redis_view_counter.place.repository.PlaceRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,9 +13,10 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class PlaceService {
     private final PlaceRepository placeRepository;
+    private final StringRedisTemplate redisTemplate;
 
     @Transactional
-    public PlaceResponseDto createPlace(PlaceCreateRequestDto request){
+    public PlaceResponseDto createPlace(PlaceCreateRequestDto request) {
         Place place = Place.builder()
                 .name(request.getName())
                 .viewCount(0L)
@@ -24,16 +26,41 @@ public class PlaceService {
     }
 
     @Transactional(readOnly = true)
-    public PlaceResponseDto getPlace(Long id){
+    public PlaceResponseDto getPlace(Long id) {
         Place place = placeRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 장소가 없습니다. id = " + id));
-        return PlaceResponseDto.from(place);
+//         Redis용
+        String key = generateViewKey(id);
+        String redisValue = redisTemplate.opsForValue().get(key);
+
+        long totalViewCount = place.getViewCount();
+
+        if(redisValue != null){
+            totalViewCount += Long.parseLong(redisValue);
+        }
+        return PlaceResponseDto.from(place, totalViewCount);
+
+//         DB용
+//        return PlaceResponseDto.from(place);
     }
 
-    @Transactional
-    public void increaseViewCount(Long id){
-        Place place = placeRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 장소가 없습니다. id = " + id));
-        place.increaseViewCount();
+    @Transactional(readOnly = true)
+    public void increaseViewCount(Long id) {
+//        DB용
+//        Place place = placeRepository.findById(id)
+//                .orElseThrow(() -> new IllegalArgumentException("해당 장소가 없습니다. id = " + id));
+//        place.increaseViewCount();
+
+//        Redis용
+        if (!placeRepository.existsById(id)){
+            throw new IllegalArgumentException("해당 장소가 없습니다. id = " + id);
+        }
+        String key = generateViewKey(id);
+        redisTemplate.opsForValue().increment(key);
+    }
+
+    //    Redis용
+    private String generateViewKey(Long placeId) {
+        return "place:view:" + placeId;
     }
 }
