29022f11e9f79d2a0688f6586a699e7af8a65bbd
jisooou <jisoou4@gmail.com>
2026-03-17 18:57:02 +0900

    feat: Redis에 저장된 조회수를 DB에 반영하는 Scheduler 구현

--- src/main/java/com/counter/redis_view_counter/RedisViewCounterApplication.java
+++ src/main/java/com/counter/redis_view_counter/RedisViewCounterApplication.java
@@ -2,7 +2,9 @@ package com.counter.redis_view_counter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class RedisViewCounterApplication {
 
